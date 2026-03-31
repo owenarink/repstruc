1c842d35ea5d7e794cf6c6c93cf9c492ae1fa09d
Owen Arink <owenandrearink@gmail.com>
2026-03-31 16:56:00 +0200

    Update Homebrew formula checksum

--- Formula/repstruc.rb
+++ Formula/repstruc.rb
@@ -4,7 +4,7 @@ class Repstruc < Formula
   desc "Generate and maintain README structure blocks for every folder in a repository"
   homepage "https://github.com/owenarink/repstruc"
   url "https://github.com/owenarink/repstruc/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_SHA256"
+  sha256 "3a52603c78ce438591c396dbe08e1c83be6c1db6a69c3a4099a62d63cb8114b0"
   license "MIT"
 
   depends_on "python@3.12"
